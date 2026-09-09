375f19822ac55b5c65dd473c8ca67db39cda1fd5
Mohamed Mamdouh Fathy <mamdouhmohamed220@gmail.com>
2026-09-10 02:12:49 +0300

    CSharp - Day1

---
+++ CSharp - Day1 - 3dPoints and Fractions/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_labsDay01_3dPointAndFraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            _3dPoint p1 = new _3dPoint(8.67, 5.34, 9.36);
+            _3dPoint p2 = new _3dPoint(3.12, 1.00, 4.50);
+
+            // Call the method and pass the two point objects
+            double distance = p1.CalculateDistance(p1, p2);
+            Console.WriteLine($"The Distance is : {distance}");
+        }
+    }
+}
