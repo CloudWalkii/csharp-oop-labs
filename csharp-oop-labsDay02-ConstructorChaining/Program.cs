a05d45007c76f82597822f6a93fcc25e2fcc8736
Mohamed Mamdouh Fathy <mamdouhmohamed220@gmail.com>
2026-09-10 02:03:15 +0300

    Add files via upload

---
+++ csharp-oop-labsDay02-ConstructorChaining/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_labsDay02_ConstructorChaining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction res = f1.Add(new Fraction(3, 2));
+            res.Display();
+        }
+    }
+}
