70995a9e6471ec40bafe85fa4614cf31711562a0
Mohamed Mamdouh Fathy <mamdouhmohamed220@gmail.com>
2026-09-10 02:13:30 +0300

    CSharp - Day 2

---
+++ CSharp - Day 2 - Constructors Chaining/3dPoint.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_labsDay02_ConstructorChaining
+{
+    class _3dPoint
+    {
+        public double X, Y, Z;
+        public _3dPoint()
+        {
+            X = 0;
+            Y = 0;
+            Z = 0;
+        }
+        public _3dPoint(double x, double y)
+        {
+            X = x;
+            Y = y;
+
+        }
+
+        public _3dPoint(double x, double y, double z) : this(x, y)
+        {
+            Z = z;
+        }
+        public void DisplayPoints()
+        {
+            Console.WriteLine($"X = {X} \tY = {Y} \tZ = {Z}");
+        }
+        public double CalculateDistance(_3dPoint P1, _3dPoint P2)
+        {
+            return Math.Abs(P1.X - P2.X);
+        }
+    }
+}
